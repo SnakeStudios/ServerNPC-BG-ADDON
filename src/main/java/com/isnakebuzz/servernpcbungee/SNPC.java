4d70e313348f269f9cf1b130b00465d5a32c6055
iSnakeBuzz <contacto@isnakebuzz.com>
2020-09-22 05:53:00 -0300

    Registered event!!!

--- src/main/java/com/isnakebuzz/servernpcbungee/SNPC.java
+++ src/main/java/com/isnakebuzz/servernpcbungee/SNPC.java
@@ -1,16 +1,11 @@
 package com.isnakebuzz.servernpcbungee;
 
+import com.isnakebuzz.servernpcbungee.Listener.MessagesListener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class SNPC extends Plugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        this.getProxy().getPluginManager().registerListener(this, new MessagesListener());
     }
 }
